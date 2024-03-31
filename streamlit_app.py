26c4311bb7dae79c75993ed372a8bf71f6c95869
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 18:52:35 +0000

    ed

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,7 +205,8 @@ with col1:
                                     font-size: 10px;
                                     padding: 10px; 
                                     line-height: 23px;
-                                    border-radius: 7px; 
+                                    border-radius: 7px;
+                                    margin-right: 3em; 
                                     border: 3.5px solid {border_color}; /* Adding 2px border with gold color */
                                     font-style: italic; 
                                     >
