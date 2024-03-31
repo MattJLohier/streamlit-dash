bfb8581a1c00e65768c3e655058b3590495962b6
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 17:57:35 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -202,10 +202,9 @@ with col1:
                                     color: rgba({wch_colour_font[0]}, 
                                             {wch_colour_font[1]}, 
                                             {wch_colour_font[2]}, 0.75); 
-                                    font-size: 20px;
+                                    font-size: {fontsize}px;
                                     padding: 10px; 
                                     line-height: 23px;
-                                    font-weight: 800;
                                     border-radius: 7px; 
                                     border: 3.5px solid {border_color}; /* Adding 2px border with gold color */
                                     font-style: italic; 
