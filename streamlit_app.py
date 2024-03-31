77d0ff25d3bdcd97a9da9a81bc902bf5f5720b12
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 09:06:27 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ with col1:
     combined_df.reset_index(drop=True, inplace=True)
     combined_df.drop_duplicates(inplace=True)
     combined_df['Certification Date'] = combined_df['Certification Date'].astype(str).str[:10]
-
+    df3_modified.drop_duplicates(inplace=True)
 
     # Show only the newest 5 records
     newest_records = combined_df.head(15)
