0eed60942b38fd6e8f4d8af23c26fbe56616e220
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 08:40:33 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,6 +138,7 @@ with col1:
     combined_df['Certification Date'] = combined_df['Certification Date'].astype(str).str[:10]
 
 
+
     # Sort the combined dataframe by "Certification Date" in descending order
     combined_df['Certification Date'] = pd.to_datetime(combined_df['Certification Date'], errors='coerce')  # Convert to datetime format
     combined_df.sort_values(by='Certification Date', ascending=False, inplace=True)
