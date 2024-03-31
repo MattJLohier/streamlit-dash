3de56a226591b3cdbafff171900d0ac53a018361
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 21:55:22 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -337,7 +337,7 @@ with col2:
         y=alt.Y('Count', title='Count')
     ).properties(
         width=700,
-        height=475
+        height=500
     )
 
     # Displaying the chart using st.write()
