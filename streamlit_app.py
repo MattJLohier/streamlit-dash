331738f9b2a76379f2b4a529fb34997d4531bbaf
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 21:43:57 +0000

    ed

--- streamlit_app.py
+++ streamlit_app.py
@@ -289,7 +289,7 @@ with col2:
             title = "Certification Spotted"
 
         # Set background color and border color based on action
-        bg_color = '#FFCCCC' if metric_label == 'Removed' else '#CCFFCC' if metric_label == 'Added' else '#F5F5F5'  # Light red for Removed, light green for Added, light gray otherwise
+        bg_color = '#FFCCCC' if metric_label == 'Removed' else '#dbffdb' if metric_label == 'Added' else '#F5F5F5'  # Light red for Removed, light green for Added, light gray otherwise
         border_color = '#FF0000' if metric_label == 'Removed' else '#1fd100' if metric_label == 'Added' else '#808080'  # Red for Removed, Green for Added, Gray otherwise
 
         htmlstr = f"""<p style='background-color: {bg_color}; 
