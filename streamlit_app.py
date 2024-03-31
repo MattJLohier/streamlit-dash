8c8f780f4e18ac3f15f9114e20b565e446bbb24c
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 07:46:15 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -142,7 +142,7 @@ with col2:
         metric_value = row['Product Name']
         metric_delta = str(count)
         # Change color based on action
-        delta_color = 'inverse' if metric_label == 'Added' else 'normal' if metric_label == 'Removed' else 'normal'
+        delta_color = 'normal' if metric_label == 'Added' else 'inverse' if metric_label == 'Removed' else 'normal'
         st.metric(label=metric_label, value=metric_value, delta=metric_delta, delta_color=delta_color)
 
 
