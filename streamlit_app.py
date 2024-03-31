c79ebb7805a7f9f3b780777326d905489ecdd8cd
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 05:27:35 +0000

    column

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,14 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
-import streamlit as st
 from st_files_connection import FilesConnection
 
 st.set_page_config(
     page_title="Scooper Dashboard",
     page_icon="🖨️",
     layout="wide",
-    initial_sidebar_state="expanded")
+    initial_sidebar_state="expanded"
+)
 
 alt.themes.enable("dark")
 
@@ -17,95 +17,102 @@ st.title('Scooper Dashboard 🖨️')
 st.markdown('**What can this app do?**')
 st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
 st.divider()
-st.header('Certifications 📝')
-st.subheader('Energy Star ⚡')
 
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.connection('s3', type=FilesConnection)
-df = conn.read("scoops-finder/baseline2.csv", input_format="csv", ttl=600)
+# Define columns
+col1, col2 = st.columns(2)
 
-# Specify the columns to keep
-columns_to_keep = ['brand_name', 'model_name', 'product_type', 'marking_technology',
-                   'color_capability', 'date_available_on_market', 'date_qualified',
-                   'markets', 'monochrome_product_speed_ipm_or_mppm']
+# Left Column: Product Registrations
+with col1:
+    st.header('Certifications 📝')
+    st.subheader('Energy Star ⚡')
 
-# Create a new DataFrame with only the specified columns
-new_df = df[columns_to_keep]
+    # Create connection object and retrieve file contents.
+    # Specify input format is a csv and to cache the result for 600 seconds.
+    conn = st.connection('s3', type=FilesConnection)
+    df = conn.read("scoops-finder/baseline2.csv", input_format="csv", ttl=600)
 
-# Rename the columns
-new_df = new_df.rename(columns={
-    'brand_name': 'Brand',
-    'model_name': 'Model',
-    'product_type': 'Product Type',
-    'marking_technology': 'Technology',
-    'color_capability': 'Color/Mono',
-    'date_available_on_market': 'Date Available',
-    'date_qualified': 'Date Qualified',
-    'markets': 'Target Markets',
-    'monochrome_product_speed_ipm_or_mppm': 'Print Speed'
-})
+    # Specify the columns to keep
+    columns_to_keep = ['brand_name', 'model_name', 'product_type', 'marking_technology',
+                       'color_capability', 'date_available_on_market', 'date_qualified',
+                       'markets', 'monochrome_product_speed_ipm_or_mppm']
 
-# Specify the brands to filter
-brands_to_show = ["Canon", "Brother", "HP", "Epson", "Konica Minolta", "Kyocera", 
-                  "Lexmark", "Ricoh", "Sharp", "Toshiba", "Xerox", "Pantum", "Fujifilm", "HP Inc."]
+    # Create a new DataFrame with only the specified columns
+    new_df = df[columns_to_keep]
 
-# Specify the product types to filter
-product_types_to_show = ['Printers', 'Multifunction Devices (MFD)']
+    # Rename the columns
+    new_df = new_df.rename(columns={
+        'brand_name': 'Brand',
+        'model_name': 'Model',
+        'product_type': 'Product Type',
+        'marking_technology': 'Technology',
+        'color_capability': 'Color/Mono',
+        'date_available_on_market': 'Date Available',
+        'date_qualified': 'Date Qualified',
+        'markets': 'Target Markets',
+        'monochrome_product_speed_ipm_or_mppm': 'Print Speed'
+    })
 
-# Cut off the "Date Available" and "Date Qualified" columns to remove anything after the first 10 digits
-new_df['Date Available'] = new_df['Date Available'].str[:10]
-new_df['Date Qualified'] = new_df['Date Qualified'].str[:10]
+    # Specify the brands to filter
+    brands_to_show = ["Canon", "Brother", "HP", "Epson", "Konica Minolta", "Kyocera",
+                      "Lexmark", "Ricoh", "Sharp", "Toshiba", "Xerox", "Pantum", "Fujifilm", "HP Inc."]
 
-# Filter the new DataFrame to only include specified brands, product types, and hide entries with "Label Printer" in the Model column
-filtered_df = new_df[(new_df['Brand'].isin(brands_to_show)) & 
-                     (new_df['Product Type'].isin(product_types_to_show)) & 
-                     (~new_df['Model'].str.contains('Model Printer|Label Printer', case=False))]
+    # Specify the product types to filter
+    product_types_to_show = ['Printers', 'Multifunction Devices (MFD)']
 
-# Sort the filtered DataFrame by Date Available (Newest to Oldest)
-filtered_df.sort_values(by='Date Available', ascending=False, inplace=True)
+    # Cut off the "Date Available" and "Date Qualified" columns to remove anything after the first 10 digits
+    new_df['Date Available'] = new_df['Date Available'].str[:10]
+    new_df['Date Qualified'] = new_df['Date Qualified'].str[:10]
 
-# Print filtered results.
-st.write(filtered_df)
+    # Filter the new DataFrame to only include specified brands, product types, and hide entries with "Label Printer" in the Model column
+    filtered_df = new_df[(new_df['Brand'].isin(brands_to_show)) &
+                         (new_df['Product Type'].isin(product_types_to_show)) &
+                         (~new_df['Model'].str.contains('Model Printer|Label Printer', case=False))]
 
-# Add metric to show latest 3 records based on product name
-#latest_records = filtered_df.head(3)['Model'].tolist()
-#st.metric("Latest 3 Products", ", ".join(latest_records))
+    # Sort the filtered DataFrame by Date Available (Newest to Oldest)
+    filtered_df.sort_values(by='Date Available', ascending=False, inplace=True)
 
-st.subheader('WiFi Alliance 📶')
-# Create connection object and retrieve file contents.
-# Specify input format is a csv and to cache the result for 600 seconds.
-conn = st.connection('s3', type=FilesConnection)
-df2 = conn.read("scoops-finder/baseline3.csv", input_format="csv", ttl=600)
+    # Print filtered results.
+    st.write(filtered_df)
 
-# Keep only the desired columns
-df2_modified = df2[["CID", "Date of Last Certification", "Brand", "Product", "Model Number"]]
+    # Add metric to show latest 3 records based on product name
+    # latest_records = filtered_df.head(3)['Model'].tolist()
+    # st.metric("Latest 3 Products", ", ".join(latest_records))
 
-# Filter by specified brands
-df2_modified = df2_modified[df2_modified["Brand"].isin(brands_to_show)]
+    st.subheader('WiFi Alliance 📶')
+    # Create connection object and retrieve file contents.
+    # Specify input format is a csv and to cache the result for 600 seconds.
+    conn = st.connection('s3', type=FilesConnection)
+    df2 = conn.read("scoops-finder/baseline3.csv", input_format="csv", ttl=600)
 
-# Sort the dataframe by "Date of Last Certification", from newest to oldest
-df2_modified.sort_values(by="Date of Last Certification", ascending=False, inplace=True)
+    # Keep only the desired columns
+    df2_modified = df2[["CID", "Date of Last Certification", "Brand", "Product", "Model Number"]]
 
-# Write the modified dataframe
-st.write(df2_modified)
+    # Filter by specified brands
+    df2_modified = df2_modified[df2_modified["Brand"].isin(brands_to_show)]
 
+    # Sort the dataframe by "Date of Last Certification", from newest to oldest
+    df2_modified.sort_values(by="Date of Last Certification", ascending=False, inplace=True)
 
-st.subheader('EPEAT 🌎')
-conn = st.connection('s3', type=FilesConnection)
-df3 = conn.read("scoops-finder/baseline4.csv", input_format="csv", ttl=600)
+    # Write the modified dataframe
+    st.write(df2_modified)
 
-# Keep only the desired columns
-df3_modified = df3[["Id", "Registered On", "Product Type", "Product Name", "Manufacturer"]]
+    st.subheader('EPEAT 🌎')
+    conn = st.connection('s3', type=FilesConnection)
+    df3 = conn.read("scoops-finder/baseline4.csv", input_format="csv", ttl=600)
 
-# Filter by specified brands
-df3_modified = df3_modified[df3_modified["Manufacturer"].isin(brands_to_show)]
+    # Keep only the desired columns
+    df3_modified = df3[["Id", "Registered On", "Product Type", "Product Name", "Manufacturer"]]
 
-# Sort the dataframe by "Date of Last Certification", from newest to oldest
-df3_modified.sort_values(by="Registered On", ascending=False, inplace=True)
+    # Filter by specified brands
+    df3_modified = df3_modified[df3_modified["Manufacturer"].isin(brands_to_show)]
 
-# Write the modified dataframe
-st.write(df3_modified)
+    # Sort the dataframe by "Date of Last Certification", from newest to oldest
+    df3_modified.sort_values(by="Registered On", ascending=False, inplace=True)
 
-st.divider()
-st.header('Placements 💡')
+    # Write the modified dataframe
+    st.write(df3_modified)
+
+# Right Column: Placements
+with col2:
+    st.header('Placements 💡')
+    # Add your placements data here
