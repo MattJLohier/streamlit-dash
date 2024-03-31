0329f915dc5c54bf34d14803b0996c4cd3d47b12
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 17:35:03 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -217,6 +217,7 @@ with col1:
                                     <span style='font-size: 16px; color: #555; padding-left: 10px;'><b>Brand:</b> {brand}</span>
                                     <span style='font-size: 16px; color: #555; padding-left: 10px;'><b>Product Name:</b> {product_name}</span>
                                     <span style='font-size: 16px; color: #555; padding-left: 10px;'><b>Product Type:</b> {product_type}</span>
+                                    <br>
                                     <span style='font-size: 16px; color: #555; padding-left: 10px; padding-bottom: 0px; margin: 0px;'><b>Certification Date:</b> {certification_date}</span>
                                     <span style='font-size: 16px; color: #555; padding-left: 10px;'><b>Source:</b> {source}</span>
                                     </p>"""
