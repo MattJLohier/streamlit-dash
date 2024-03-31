ad4ca53582f17a29d96e2923f391947c8e863b7a
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 22:54:59 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ alt.themes.enable("dark")
 
 st.title('Scooper Dashboard 🖨️')
 st.markdown('**Welcome to Scooper Dashboard**')
-st.info('This is the official Scooper dashboard. Scooper is a Python tool hosted on AWS (Lambda/S3/EC2) that uses Selenium and Pandas to scrape new product certifications and placements from official manufacturer websites.') 
+st.info('Scooper is a Python tool hosted on AWS (Lambda/S3/EC2) that uses Selenium and Pandas to scrape new product certifications and placements from official manufacturer websites.') 
 st.caption('Scooper currently scrapes certifications from EnergyStar, WiFi Alliance, and the EPEAT registry, and checks for new products on the official sites of HP, Canon, Kyocera, Konica Minolta, Lexmark, Ricoh, Sharp, Toshiba, Xerox, and Fujifilm')
 st.caption('Created By Matt Lohier')
 st.divider()
