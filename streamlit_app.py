b607c68a64c009f2ee46820e6b9f82c5e20e6382
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 21:23:10 +0000

    ed

--- streamlit_app.py
+++ streamlit_app.py
@@ -330,7 +330,7 @@ with col2:
             st.markdown(lnk + htmlstr, unsafe_allow_html=True)
 
     st.subheader('Current Placement Count By Brand')
-    st.bar_chart(df5.set_index('Brand')['Count'], width=200, height=550, color='#24AABE')
+    st.bar_chart(df5.set_index('Brand')['Count'], width=200, height=500, color='#24AABE')
     st.subheader('Placement Changelog')
     st.write(df4)
     st.subheader('Brand Totals Changelog')
