77267f23040a56dc1554fbbfe163500593bfea5d
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 08:46:12 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,15 +135,21 @@ with col1:
 
     # Concatenate the dataframes
     combined_df = pd.concat([filtered_df, df2_modified, df3_modified], ignore_index=True)
+    # Assuming "Certification Date" is already in string format
+    # If not, convert it to string before truncating
+    combined_df['Certification Date'] = combined_df['Certification Date'].astype(str)
+
+    # Truncate the date to keep only the first 10 characters
     combined_df['Certification Date'] = combined_df['Certification Date'].str[:10]
+
+    # Convert the truncated date to datetime format
+    combined_df['Certification Date'] = pd.to_datetime(combined_df['Certification Date'], errors='coerce')
 
     # Sort the combined dataframe by "Certification Date" in descending order
-    combined_df['Certification Date'] = pd.to_datetime(combined_df['Certification Date'], errors='coerce')  # Convert to datetime format
     combined_df.sort_values(by='Certification Date', ascending=False, inplace=True)
 
-
+    # Reset index
     combined_df.reset_index(drop=True, inplace=True)
-    combined_df['Certification Date'] = combined_df['Certification Date'].str[:10]
 
     # Show only the newest 5 records
     newest_records = combined_df.head(10)
