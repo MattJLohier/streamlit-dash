3bfb60bb017d0e575a13c29b25bf3d50ed867eb1
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 22:02:24 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -346,6 +346,7 @@ with col2:
     st.subheader('Placement Changelog')
     
     df4_sorted = df4.sort_values(by='Date Detected', ascending=False)
+    df4_sorted.reset_index(drop=True, inplace=True)
     st.dataframe(df4_sorted, width=1200, height=375)
 
 
