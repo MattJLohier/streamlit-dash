fb1de3d9b7c5f5dec4de8b6e621284404ebcd8ce
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 16:30:56 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -167,7 +167,9 @@ with col1:
     st.write(estardf)
     st.subheader('WiFi Alliance 📶')
     st.write(df2_modified)
+    cdf2_modified.sort_values(by='Certification Date', ascending=False, inplace=True)
     st.subheader('EPEAT 🌎')
+    
     st.write(df3_modified)
 # Right Column: Placements
 # Right Column: Placements
