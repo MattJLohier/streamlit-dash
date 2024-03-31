8259fda591df7f0d54a5d366bc9c017a489b7d90
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 20:41:27 +0000

    ed

--- streamlit_app.py
+++ streamlit_app.py
@@ -312,7 +312,7 @@ with col2:
                                 padding-top: 18px; 
                                 >
                                 <span style='font-size: 20px; padding-left: 12px;
-                                    margin-top: 10px;'><b> {title} {emoji} </b></span>
+                                    margin-top: 10px;'><b style='padding-left:10px'> {title} {emoji} </b></span>
                                     <br>
                                     <span style='font-size: 16px; color: #555; padding-left: 10px;'><b style='font-weight: 800;' >Brand:</b> {brand}</span>
                                     <br>
