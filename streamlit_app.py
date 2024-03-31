f97e29c9274c2ae86bcb8204b03ba4f287d8dbed
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-03-31 18:59:50 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ alt.themes.enable("dark")
 
 st.title('Scooper Dashboard 🖨️')
 st.markdown('**Welcome to Scooper Dashboard**')
-st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
+st.info('This is the official dashboard for the Scooper Tool. Scooper is a python tool hosted on AWS (Lambda/S3/EC2) that uses the Python selenium and pandas libraries to scrape for new product certifications and placements on official manufacturer websites.')
 st.divider()
 
 # Define columns
