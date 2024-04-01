4f08753db3f2f72791a86abee51fa2786d9c3969
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-04-01 19:44:04 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -357,7 +357,6 @@ with col2:
 
     #st.write(df4_sorted)
     st.subheader('Brand Totals Changelog')
-    st.subheader('Brand Totals Changelog 2')
     #df7 = df7[-10:]
     df7.drop_duplicates(subset=["Brand", "Date", "Count"], inplace=True)
     df7 = df7.sort_values(by='Date', ascending=False).reset_index(drop=True)
