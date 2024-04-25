74e97949f743ef861f4a54bae24822f7b61dd697
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-04-25 22:08:08 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -441,7 +441,7 @@ def display_login_form():
     col1, col2, col3 = st.columns([1, 2, 1])
     
     with col2:  # Middle column for the form
-        st.markdown("<h1 style='text-align: center;'><span style='#66b2b2: teal;'>Scooper</span> Dashboard 🖨️</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center;'><span style='color: #66b2b2;'>Scooper</span> Dashboard 🖨️</h1>", unsafe_allow_html=True)
         with st.form(key='login_form'):
             # Input fields for username and password
             username = st.text_input("Username")
