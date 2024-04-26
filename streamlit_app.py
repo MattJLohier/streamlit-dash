465c8f8a970e2e4128df7a872e97aa5161413eec
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-04-26 08:46:45 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,11 +82,11 @@ def sidebar():
 
     }
     [data-testid="stSidebarUserContent"] .stButton button:active {
-        background-color: #0056b3;       /* Set active state background color */
+        background-color: red;       /* Set active state background color */
         color: white;                    /* Set text color in active state */
     }
     [data-testid="stSidebarUserContent"] .stButton button:focus {
-        background-color: #0056b3;       /* Set active state background color */
+        background-color: red;       /* Set active state background color */
         color: white;                    /* Set text color in active state */
     }
     </style>
