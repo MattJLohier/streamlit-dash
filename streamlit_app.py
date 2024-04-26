41273bb1bfca1bbbfa64c1d72f325014257fe1fd
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-04-26 21:14:54 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -382,6 +382,7 @@ def show_raw_data():
     # Code to display raw data
     st.subheader('Raw Placement Data')
 
+
 def show_changelog():
     # Code to display changelog
     st.subheader('Changelog')
@@ -696,7 +697,15 @@ def show_recent_cert():
 def show_raw_data_cert():
     st.subheader('Raw Certification Data')
     st.subheader('Energy Star ⚡')
+    conn = st.connection('s3', type=FilesConnection)
+    df_raw_certs = conn.read("scoops-finder/tracking.csv", input_format="csv", ttl=600)
+    st.write(df_raw_certs)
+
+
     st.subheader('WiFi Alliance 📶')
+    conn = st.connection('s3', type=FilesConnection)
+     = conn.read("scoops-finder/tracking.csv", input_format="csv", ttl=600)
+
     st.subheader('EPEAT 🌎')
 
 def show_changelog_cert():
