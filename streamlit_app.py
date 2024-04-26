94234b7d921857fe73e62cd1ae51f23a1c7a702f
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-04-26 06:54:35 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -409,8 +409,9 @@ def show_recent_cert():
     .grid-container {
         display: grid;
         grid-template-columns: repeat(3, 1fr); /* Maintains 3 columns */
-        grid-gap: 30px; /* Space between cards */
+        grid-gap: 10px; /* Space between cards */
         padding: 10px;
+        gap: 1rem;
         width: auto; /* Adjust based on the actual space available or use 100% if it should be fully responsive */
     }
 
