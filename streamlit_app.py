d04831d950d7ec7752f7ff907ea50963a4b2b42b
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-04-26 07:57:02 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -523,7 +523,7 @@ def show_recent_cert():
 
     .card {
         height: auto;
-        min-height: 100%;
+        min-height: 120px;
         position: relative;
         width: 100%; /* This makes each card responsive within its grid column */
         display: flex;
