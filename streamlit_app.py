5842ec5a507203767cf512bad9287693e2ea8f16
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-04-26 08:51:17 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -684,7 +684,7 @@ def show_recent_cert():
 
 
 def show_raw_data_cert():
-    st.header('Raw Certification Data 📝')
+    st.subheader('Raw Certification Data 📝')
     st.subheader('Energy Star ⚡')
     st.subheader('WiFi Alliance 📶')
     st.subheader('EPEAT 🌎')
