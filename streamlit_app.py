b3d267f3f49a95646a021f29acf92697bab377c8
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-04-27 07:01:58 +0000

    EC

--- streamlit_app.py
+++ streamlit_app.py
@@ -391,7 +391,7 @@ def show_changelog():
 
     # Reshape the DataFrame
     pivoted_df = placement_changelog.pivot_table(index='Date', columns='Brand', values='Count', fill_value=0)
-    pivoted_df = pivoted_df.sort_values('Date', ascending=False)
+    #pivoted_df = pivoted_df.sort_values('Date', ascending=False)
 
     # Display the transposed DataFrame
     st.write(pivoted_df)
