14321ff626cbe7f4a5b34c1cfbb28c2dd83db285
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-04-27 08:57:03 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -461,6 +461,18 @@ def show_insights():
     # Streamlit UI for the visualization
     st.title('Brand Distribution on Latest Date')
 
+
+    st.title('Brand Counts Over Time')
+    all_brands = list(pivoted_df.columns)
+    selected_brands = st.multiselect('Select Brands', all_brands, default=all_brands)
+
+    # Filter data based on selected brands
+    filtered_data = pivoted_df[selected_brands]
+
+    # Display the bar chart
+    st.bar_chart(filtered_data, height=500)
+
+
     # Creating two columns
     col1, col2 = st.columns(2)
 
