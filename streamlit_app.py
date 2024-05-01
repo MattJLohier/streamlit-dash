8222c7aebd1bf94966b1735900a1b4cc9756c372
MattJLohier <30995440+MattJLohier@users.noreply.github.com>
2024-05-01 20:47:35 +0000

    ec

--- streamlit_app.py
+++ streamlit_app.py
@@ -776,7 +776,7 @@ def show_changelog_cert():
     df_epeat_changelog = placement_tracking2
 
     columns_to_keep2 = ["Registered On", "Product Name", "Manufacturer", "Climate+", "Product Category", 
-                    "Product Type", "Status", "Registered In", "Total Score", "EPEAT Tier", "Date"]
+                    "Product Type", "Status", "Registered In", "Total Score", "EPEAT Tier", "Date Detected"]
 
 
     st.write(df_epeat_changelog)
